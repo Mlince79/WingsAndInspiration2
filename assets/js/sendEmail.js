45bdf479b11ddb16371d594ffbe6d9810f2f1382
Marisol <marisol.lince@gmail.com>
2020-01-04 12:50:33 +0000

    email js

--- assets/js/sendEmail.js
+++ assets/js/sendEmail.js
@@ -2,13 +2,17 @@
 function sendMail(contactForm) {
     emailjs.send("gmail", "denisse_olsson", {
         "from_name": contactForm.name.value,
-        "from_email": contactForm.email.value,
-        "meeting_request": contactForm.text.value,
-})
-.then(
-    function(response) {
-        console.log("SUCCESS", response);
-    },
-    function(error) {
-        console.log("FAILED", error);
-    });
+        "from_email": contactForm.emailaddress.value,
+        "meeting_request": contactForm.meeting.value,
+    })
+    .then(
+        function (response) {
+            console.log("SUCCESS", response);
+        },
+        function (error) {
+            console.log("FAILED", error);
+        }
+    );
+    return false;
+}
+
