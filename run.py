cd75b60cb1f4edb1d9031a2eb9ee40192fa69829
Marisol <marisol.lince@gmail.com>
2020-06-05 18:13:09 +0000

    templates register, login, layout, blog, forms, images

---
+++ run.py
@@ -0,0 +1,8 @@
+from flaskblog import app
+import os
+
+    
+if __name__ == '__main__':
+    app.run(host=os.environ.get('IP', "0.0.0.0"),
+            port=int(os.environ.get('PORT', "5000")),
+            debug=True)
